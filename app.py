9d0a97288748852724d74ad1954e9d13f853a01b
codewithshriraj <erwadkars@gmail.com>
2026-04-14 14:26:03 +0530

    Sanitize credential defaults and remove hardcoded password

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def get_db_connection():
         host=os.getenv("MYSQL_HOST", "localhost"),
         port=int(os.getenv("MYSQL_PORT", "3306")),
         user=os.getenv("MYSQL_USER", "root"),
-        password=os.getenv("MYSQL_PASSWORD", "shri@123"),
+        password=os.getenv("MYSQL_PASSWORD", ""),
         database=os.getenv("MYSQL_DATABASE", "course_db"),
     )
 
